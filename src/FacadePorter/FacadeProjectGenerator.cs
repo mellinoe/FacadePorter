00ad43aa9f6363143380b2c6867ac230783c4a02
Eric Mellino <erme@microsoft.com>
2015-12-03 14:04:14 -0800

    Handle assembly references (somewhat)

--- src/FacadePorter/FacadeProjectGenerator.cs
+++ src/FacadePorter/FacadeProjectGenerator.cs
@@ -56,17 +56,47 @@ namespace FacadePorter
                 configurationsBlock += Environment.NewLine + DesktopConfigurations;
             }
 
+            // Special .NET Native project.json references
+            string specialNetNativeJsonText = (info.HasNetCoreForCoreClrBuild) ? SpecialNetNativeJson : string.Empty;
+
+            // targeting pack assembly references
+            string assemblyRefs = "";
+            if (info.ProjectKVersion != null || info.DesktopVersion != null)
+            {
+                assemblyRefs += Environment.NewLine + string.Format(ReferenceFormat, "mscorlib", Condition_DoesNotTargetAot);
+            }
+            if (info.DesktopVersion != null)
+            {
+                string[] desktopRefs = GetDesktopRefs(info.Name);
+                foreach (string assm in desktopRefs)
+                {
+                    assemblyRefs += Environment.NewLine + string.Format(ReferenceFormat, assm, Condition_TargetsDesktop);
+                }
+            }
+
+            if (info.ProjectNVersion != null)
+            {
+                string[] projectNRefs = GetProjectNRefs(info.Name);
+                foreach (string assm in projectNRefs)
+                {
+                    assemblyRefs += Environment.NewLine + string.Format(ReferenceFormat, assm, Condition_TargetsAot);
+                }
+            }
+
             string fileText = string.Format(
                 s_projTemplateText,
                 info.Name,
                 assemblyVersionBlock,
-                configurationsBlock);
+                configurationsBlock,
+                specialNetNativeJsonText,
+                assemblyRefs);
 
             string outputPath = Path.Combine(outputDir, info.Name, "src", "facade");
             Directory.CreateDirectory(outputPath);
             string outputFile = Path.Combine(outputPath, info.Name + ".csproj");
             File.WriteAllText(outputFile, fileText);
 
+            // Create project.json for facade project.
             string projectJsonBody = "";
             if (info.ProjectKVersion != null)
             {
@@ -98,6 +128,25 @@ namespace FacadePorter
             File.WriteAllText(jsonOutputFile, projectJsonFull);
         }
 
+        private string[] GetProjectNRefs(string name)
+        {
+            return new string[]
+            {
+                "System.Private.CoreLib",
+                "System.Private.Reflection"
+            };
+        }
+
+        private string[] GetDesktopRefs(string name)
+        {
+            return new string[]
+            {
+                "System",
+                "System.Core",
+                "System.ComponentModel.Composition"
+            };
+        }
+
         private const string ProjectKConfigurations =
 @"  <PropertyGroup Condition="" '$(Configuration)|$(Platform)' == 'Debug|AnyCPU' "" />
   <PropertyGroup Condition="" '$(Configuration)|$(Platform)' == 'Release|AnyCPU' "" />";
@@ -122,11 +171,11 @@ namespace FacadePorter
 
         private string Condition_TargetsDesktop => string.Format(ConditionFormat, "'$(TargetsDesktop)' == 'true'");
         private string Condition_TargetsAot => string.Format(ConditionFormat, "'$(IsAot)' == 'true'");
+        private string Condition_DoesNotTargetAot => string.Format(ConditionFormat, "'$(IsAot)' != 'true'");
         private string Condition_None => string.Empty;
 
-        private const string MscorlibReferenceFormat =
-@"    <TargetingPackReference Include=""mscorlib"" Condition="" '$(IsAot)' != 'true' "" />
-";
+        private const string ReferenceFormat =
+@"    <TargetingPackReference Include=""{0}""{1} />";
 
         private const string ProjectJsonFileFormat =
 @"{{
@@ -154,5 +203,12 @@ namespace FacadePorter
                 ""Microsoft.TargetingPack.Private.NetNative"": ""1.0.0-rc2-23530""
             }
         }";
+
+        private const string SpecialNetNativeJson =
+@"  <PropertyGroup Condition="" '$(IsAot)' == 'true' "">
+    <ProjectJson>NetNative\project.json</ProjectJson>
+    <ProjectLockJson>NetNative\project.lock.json</ProjectLockJson>
+  </PropertyGroup>
+";
     }
 }
