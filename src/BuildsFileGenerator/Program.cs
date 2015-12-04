963c434bff8a3d36bdefd3740068e4006bf43e1e
Eric Mellino <erme@microsoft.com>
2015-12-04 11:12:29 -0800

    Working builds file generator, very simple

--- src/BuildsFileGenerator/Program.cs
+++ src/BuildsFileGenerator/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace BuildsFileGenerator
 {
@@ -22,12 +23,66 @@ namespace BuildsFileGenerator
             }
 
             string corefxSrcPath = args[0];
-            foreach (string file in Directory.EnumerateFiles(corefxSrcPath, "*.builds", SearchOption.AllDirectories))
+            foreach (string libraryDir in Directory.EnumerateDirectories(corefxSrcPath))
             {
-                Console.WriteLine(file);
+                string projSrcDir = Path.Combine(libraryDir, "src");
+                try
+                {
+                    var projFiles = Directory.EnumerateFiles(projSrcDir, "*.csproj", SearchOption.AllDirectories).ToArray();
+                    if (projFiles.Length > 0)
+                    {
+                        CreateBuildsFile(projSrcDir, projFiles);
+                    }
+                }
+                catch (DirectoryNotFoundException) { }
             }
 
             return 0;
         }
+
+        private static void CreateBuildsFile(string projSrcDir, string[] projFiles)
+        {
+            string includesBlock = "";
+            string libName = new FileInfo(projFiles[0]).Name;
+
+            foreach (string csprojPath in projFiles)
+            {
+                string csproj = File.ReadAllText(csprojPath);
+                string relativePath = csprojPath.Substring(projSrcDir.Length + 1);
+                if (csproj.Contains("'Debug|AnyCPU'") || csproj.Contains("'Windows_Debug|AnyCPU'"))
+                {
+                    includesBlock += Environment.NewLine
+                        + string.Format(ProjectIncludeFormat, relativePath, "");
+                }
+
+                if (csproj.Contains("'netcore50aot_Debug|AnyCPU'"))
+                {
+                    includesBlock += Environment.NewLine
+                        + string.Format(ProjectIncludeFormat, relativePath, "netcore50aot");
+                }
+
+                if (csproj.Contains("'netcore50_Debug|AnyCPU'"))
+                {
+                    includesBlock += Environment.NewLine
+                        + string.Format(ProjectIncludeFormat, relativePath, "netcore50");
+                }
+
+                if (csproj.Contains("'net46_Debug|AnyCPU'"))
+                {
+                    includesBlock += Environment.NewLine
+                        + string.Format(ProjectIncludeFormat, relativePath, "net46");
+                }
+            }
+
+            string fileText = string.Format(s_buildsFileTemplateFormat, includesBlock);
+            File.WriteAllText(Path.Combine(projSrcDir, $"{libName}.builds"), fileText);
+        }
+
+        private const string ProjectIncludeFormat =
+@"    <Project Include=""{0}"">
+      <AdditionalProperties>TargetGroup={1}</AdditionalProperties>
+    </Project>";
+
+        private static readonly string s_buildsFileTemplateFormat = File.ReadAllText(Path.Combine(AppContext.BaseDirectory, "BuildsFileTemplate.xml"));
     }
 }
